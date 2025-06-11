639e21a510440b2a6b501cfc6e4571ba7af053a5
sendy <sendy@duakelinci.com>
2025-06-11 13:03:59 +0700

    Refactor code structure for improved readability and maintainability

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="../assets/img/impact-assesment.svg" alt="logo" width="200" >
+              <img ssrc="https://schooltech-file.s3.ap-southeast-2.amazonaws.com/logo-vertical-color.png" alt="logo" width="300" >
             </div>
 
             <div class="card card-primary">
