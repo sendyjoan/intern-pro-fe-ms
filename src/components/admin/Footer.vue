4ff9e6ad1de607e214eff48cc0fbc523afa3a8e2
sendy <sendy@duakelinci.com>
2025-06-11 16:58:08 +0700

    Update Footer component: change copyright year to dynamic and update design attribution for improved accuracy and relevance.

--- src/components/admin/Footer.vue
+++ src/components/admin/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauv.al/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2021 - {{currentYear}} <div class="bullet"></div> Development By <a href="https://schooltechindonesia.com">ITDB SchoolTech Indonesia</a>
         </div>
         <div class="footer-right">
           2.3.0
@@ -12,5 +12,10 @@
 <script>
 export default {
   name: 'Footer',
+    data() {
+        return {
+        currentYear: new Date().getFullYear()
+        }
+    },
 }
 </script>
